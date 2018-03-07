6e2f122c478521cc3506e436867101e7bb3cf543
Tim DiLauro <tim.di.lauro+gitkraken@gmail.com>
2018-03-07 01:10:47 -0500

    pre-override baseline of Record::parse_sub_container_display_string

--- public/plugin_init.rb
+++ public/plugin_init.rb
@@ -45,4 +45,54 @@ ArchivesSpacePublic::Application.config.after_initialize do
 
   end
 
+
+  Record
+  class Record
+
+    # pre-override baseline of Record::parse_sub_container_display_string
+    def parse_sub_container_display_string(sub_container, inst, opts = {})
+      summary = opts.fetch(:summary, false)
+      parts = []
+
+      instance_type = I18n.t("enumerations.instance_instance_type.#{inst.fetch('instance_type')}", :default => inst.fetch('instance_type'))
+
+      # add the top container type and indicator
+      if sub_container.has_key?('top_container')
+        top_container_solr = top_container_for_uri(sub_container['top_container']['ref'])
+        if top_container_solr
+          # We have a top container from Solr
+          top_container_display_string = ""
+          top_container_json = ASUtils.json_parse(top_container_solr.fetch('json'))
+          if top_container_json['type']
+            top_container_type = I18n.t("enumerations.container_type.#{top_container_json.fetch('type')}", :default => top_container_json.fetch('type'))
+            top_container_display_string << "#{top_container_type}: "
+          else
+            top_container_display_string << "#{I18n.t('enumerations.container_type.container')}: "
+          end
+          top_container_display_string << top_container_json.fetch('indicator')
+          parts << top_container_display_string
+        elsif sub_container['top_container']['_resolved'] && sub_container['top_container']['_resolved']['display_string']
+          # We have a resolved top container with a display string
+          parts << sub_container['top_container']['_resolved']['display_string']
+        end
+      end
+
+
+      # add the child type and indicator
+      if sub_container['type_2'] && sub_container['indicator_2']
+        type = I18n.t("enumerations.container_type.#{sub_container.fetch('type_2')}", :default => sub_container.fetch('type_2'))
+        parts << "#{type}: #{sub_container.fetch('indicator_2')}"
+      end
+
+      # add the grandchild type and indicator
+      if sub_container['type_3'] && sub_container['indicator_3']
+        type = I18n.t("enumerations.container_type.#{sub_container.fetch('type_3')}", :default => sub_container.fetch('type_3'))
+        parts << "#{type}: #{sub_container.fetch('indicator_3')}"
+      end
+
+      summary ? parts.join(", ") : "#{parts.join(", ")} (#{instance_type})"
+    end
+
+  end
+
 end
